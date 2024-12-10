fb4d492a743a1ab104948b76033a35d7a1bb04a8
Han Myat Thu <hanmyatthu111@gmail.com>
2024-12-10 01:47:07 +0100

    implement tiptap extension

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,11 +1,40 @@
 "use client";
 
 import { useEditor, EditorContent } from "@tiptap/react";
+
 import StarterKit from "@tiptap/starter-kit";
+import BulletList from "@tiptap/extension-bullet-list";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import Heading from "@tiptap/extension-heading";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import Image from "@tiptap/extension-image";
+import ImageResize from "tiptap-extension-resize-image";
 
 export const Editor = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      BulletList,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      Heading.configure({
+        levels: [1, 2, 3, 4, 5, 6],
+      }),
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      Image,
+      ImageResize,
+    ],
     editorProps: {
       attributes: {
         style: "padding-left: 56px; padding-right: 56px;",
@@ -13,8 +42,16 @@ export const Editor = () => {
           "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    content: "<p>Hello World!</p>",
+    content: `
+        <p>This is a basic example of implementing images. Drag to re-order.</p>
+        <img src="https://placehold.co/800x400" />
+        <img src="https://placehold.co/800x400/6A00F5/white" />
+      `,
   });
+
+  if (!editor) {
+    return null;
+  }
 
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-2 print:p-0 print:bg-white print:overflow-visible">
